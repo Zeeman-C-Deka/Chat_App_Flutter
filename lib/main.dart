8a51014accd64ab46c952640ffbd7926809d48bf
Zeeman C Deka <79737506+Zeeman-C-Deka@users.noreply.github.com>
2023-07-10 14:47:09 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:chat_app/screens/auth.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterChat',
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 64, 17, 177),
+        ),
+      ),
+      home: AuthScreen(),
+    );
+  }
+}
